f8599edab025f8445e39c4a83207e9b4b82dffee
HienD <hdduong.at.wpi.edu@gmail.com>
2017-04-05 21:55:06 -0700

    default bean is singleton

--- src/main/java/com/cave/spring/App.java
+++ src/main/java/com/cave/spring/App.java
@@ -1,6 +1,7 @@
 package com.cave.spring;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class App {
@@ -10,17 +11,25 @@ public class App {
 		//person.speak();
 
 		//ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
-		ApplicationContext context = new FileSystemXmlApplicationContext("src/main/java/com/cave/spring/beans/beans.xml");
+		//ApplicationContext context = new FileSystemXmlApplicationContext("src/main/java/com/cave/spring/beans/beans.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/cave/spring/beans/beans.xml");
 		//cave.spring.5/src/main/java/com/cave/spring/beans/beans.xml
 		
 		Person person  = (Person)context.getBean("person");
 		System.out.println(person.toString());
+		
+		person.setTaxId(1234);
+		
+		
+		Person person2  = (Person)context.getBean("person");
+		System.out.println(person2.toString());
+		
 		//person.speak();
 		
 		Address address = (Address)context.getBean("address");
 		System.out.println(address.toString());
 		
-		((FileSystemXmlApplicationContext)context).close();
+		((ClassPathXmlApplicationContext)context).close();
 		
 	}
 
